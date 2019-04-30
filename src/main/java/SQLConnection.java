31aa351fbf0096c6940834f672c4232f5f6490cb
jornvdmaat17 <33722415+jornvdmaat17@users.noreply.github.com>
2019-04-30 13:43:26 +0200

    Update SQLConnection.java

--- src/main/java/SQLConnection.java
+++ src/main/java/SQLConnection.java
@@ -6,17 +6,20 @@ import java.sql.Connection;
 
 public class SQLConnection {
 
+    private static final String SHOW_TABLES_QUERY = "SHOW TABLES";
+    private static final String COM_MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+
     private Connection connection;
 
     public SQLConnection(String url, String user, String password) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");  
+            Class.forName(COM_MYSQL_DRIVER);  
         } catch (Exception e){
-            System.out.println("com.mysql.jdbc.Driver jar not installed!!!!!");
+            System.out.println(COM_MYSQL_DRIVER + "jar not installed!!!!!");
             e.printStackTrace();
         }
         try {
-            String connectionUrl = "jdbc:mysql://" + url + "?useUnicode=true&characterEncoding=UTF-8&user="+ user +"&password=" + password;
+            final String connectionUrl = "jdbc:mysql://" + url + "?useUnicode=true&characterEncoding=UTF-8&user="+ user +"&password=" + password;
             connection = DriverManager.getConnection(connectionUrl);
             System.out.println("Connected");
         } catch (Exception e){
@@ -28,7 +31,7 @@ public class SQLConnection {
     public List<String> getAllTables(){
         List<String> tables = new ArrayList<String>();
         try  {
-            ResultSet rs = connection.prepareStatement("show tables").executeQuery();
+            ResultSet rs = connection.prepareStatement(SHOW_TABLES_QUERY).executeQuery();
             while(rs.next()){
                 tables.add(rs.getString(1));
             }
